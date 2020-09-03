1015f5dea66e4371e2e142509a051ac7a27e4e2d
MeriSg <sgmerisg@gmail.com>
2020-09-03 14:39:36 +0300

    Updates

---
+++ precache-manifest.75020a636578c586a2dc2f37754764ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d22edbd5e63bb9288f1ed51deb3d91",
+    "url": "/todo-list-ex/index.html"
+  },
+  {
+    "revision": "c896f592ccd0f4569782",
+    "url": "/todo-list-ex/static/css/2.abc8d607.chunk.css"
+  },
+  {
+    "revision": "a1791cce5f30f0d95b4d",
+    "url": "/todo-list-ex/static/css/main.f80959a1.chunk.css"
+  },
+  {
+    "revision": "c896f592ccd0f4569782",
+    "url": "/todo-list-ex/static/js/2.9aa24096.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-list-ex/static/js/2.9aa24096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1791cce5f30f0d95b4d",
+    "url": "/todo-list-ex/static/js/main.19c20fcf.chunk.js"
+  },
+  {
+    "revision": "3bbc860b05d49a5544ce",
+    "url": "/todo-list-ex/static/js/runtime-main.9c74cf15.js"
+  }
+]);
